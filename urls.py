13821525398def1bf2e84632863019b0c11dca55
Gao Chengxing <lovekobe@gmail.com>
2012-02-03 18:00:59 +0800

    add django project files

--- urls.py
+++ urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.defaults import patterns, include, url
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
+import app1
 
 urlpatterns = patterns('',
     # Examples:
@@ -14,4 +15,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    url(r'^app1/', include(app1.urls)),
+    url(r'^$', 'app1.views.home'),
 )
