3615595f0f8c1f8b1b019919de8e6006bbbf97d0
Gao Chengxing <lovekobe@gmail.com>
2012-02-07 18:14:15 +0800

    add django project files

--- app1/views.py
+++ app1/views.py
@@ -1,1 +1,5 @@
+from django.http import HttpResponse
+
 # Create your views here.
+def home(request, year='2012'):
+    return HttpResponse('hello world' + year)
--- urls.py
+++ urls.py
@@ -15,6 +15,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-    url(r'^app1/', include(app1.urls)),
+    url(r'^app1/', include('app1.urls')),
     url(r'^$', 'app1.views.home'),
 )
